569d652ef2f1f9dd1f5905df7ae3bc50fbdcc83a
chee <chee@snaek.org>
2017-07-13 07:11:10 +0100

    add promise/pink

---
+++ play/promise/pink.js
@@ -0,0 +1,8 @@
+const {discover, setColor, pair, stop = process.exit} = require('../..')
+
+const setColorWithDispatch = (...color) => dispatch => dispatch(setColor(...color, 0xff))
+
+discover()
+  .then(pair)
+  .then(setColorWithDispatch(0xff, 0x2a, 0x50))
+  .then(stop)
