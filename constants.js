45c60b5ca4508e6588b7af70b5ef4b63e5f7972c
chee <chee@snaek.org>
2017-08-05 13:05:55 +0100

    find the service by its uuid instead of hoping for consistence

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 module.exports = {
   DEFAULT_NAME: 'Smart Light',
+
   // find your password out by connecting with the app while running `adb lolcat`
   DEFAULT_PASSWORD: '234',
-  COMMAND_CHARACTERISTIC_UUID: '',
-  PAIR_CHARACTERISTIC_UUID: ''
+  CONTROL_SERVICE_UUID: '000102030405060708090a0b0c0d1910'
 }
